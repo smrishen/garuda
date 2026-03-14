b9cd1be840224c66809edd03fa31c4523f78c9b7
Kannika482 <kannikaholla@gmail.com>
2026-03-14 12:09:09 +0530

    Update backend to serve static files

--- sserver.js
+++ sserver.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
+app.use(express.static(__dirname));
 
 const quiz = [
 
